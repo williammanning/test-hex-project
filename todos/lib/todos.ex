5ae48a47e300b0ae05a6eac083f5bc4d08e0031e
William Manning <williammanning@gmail.com>
2024-02-01 21:20:34 -0800

    First build

---
+++ todos/lib/todos.ex
@@ -0,0 +1,18 @@
+defmodule Todos do
+  @moduledoc """
+  Documentation for `Todos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Todos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
